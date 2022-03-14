023c96efa7a9a1089b03808623990a372090915b
TheAstralShade <81994529+TheAstralShade@users.noreply.github.com>
2022-03-13 20:31:03 -0500

    Further evaluation accuracy functionality

--- client/src/review.js
+++ client/src/review.js
@@ -1,44 +1,75 @@
 // We want the value being sent. To be zero. The higher the sent value, the worse the health summary.
 
 // 70 times a week for changes.
-export function gradeBath(test1){
-    var average = 70;
-    var comp = average - test1;
+export function gradeBath(test1, age){
+    var average;
     var num;
-    console.log(test1)
+    var timesPerDay;
+    //console.log(test1)
 
+    // Switch to determine the age accuracy for grading
+    switch(true) {
+        case age == 0:
+            average = 21;
+            timesPerDay = 3;
+            break;
+        case age == 1:
+            average = 14;
+            timesPerDay = 2;
+            break;
+        case age == 2:
+            average = 14;
+            timesPerDay = 2;
+            break;
+        case age == 3:
+            average = 14;
+            timesPerDay = 2;
+            break;
+        case age == 4:
+            average = 7;
+            timesPerDay = 1;
+            break;
+        case age == 5:
+            average = 7;
+            timesPerDay = 1;
+            break;
+    }
+
+    var comp = average - test1;
+
+    // Switch to evaluate the average value and return the grade value
     switch(true) {
         case comp <= 0:
             num = 425
             break;
-        case comp >= 7 && comp < 14:
+        case comp >= (0.7*timesPerDay) && comp < (1.4*timesPerDay):
             num = 510;
             break;
-        case comp >= 14 && comp < 21:
+        case comp >= (1.4*timesPerDay) && comp < (2.1*timesPerDay):
             num = 595;
             break;
-        case comp >= 21 && comp < 28:
+        case comp >= (2.1*timesPerDay) && comp < (2.8*timesPerDay):
             num = 680;
             break;
-        case comp >= 28 && comp < 35:
+        case comp >= (2.8*timesPerDay) && comp < (3.5*timesPerDay):
             num = 765;
             break;
-        case comp >= 35 && comp < 42:
+        case comp >= (3.5*timesPerDay) && comp < (4.2*timesPerDay):
             num = 850;
             break;
-        case comp >= 42 && comp < 48:
+        case comp >= (4.2*timesPerDay) && comp < (4.8*timesPerDay):
             num = 935;
             break;
-        case comp >= 48 && comp < 56:
+        case comp >= (4.8*timesPerDay) && comp < (5.6*timesPerDay):
             num = 1020;
             break;
-        case comp >= 56 && comp < 63:
+        case comp >= (5.6*timesPerDay) && comp < (6.3*timesPerDay):
             num = 1105;
             break;
-        case comp >= 63 && comp < 70:
+        case comp >= (6.3*timesPerDay) && comp < (7.0*timesPerDay):
             num = 1190;
             break;
-        case comp >= 70:
+        case comp >= (7.0*timesPerDay):
             num = 1275;
             break;
     }
@@ -47,44 +78,73 @@ export function gradeBath(test1){
     return i;
 }
 
-export function gradeSleep(test2){
-    var average = 16;
-    var comp = average - test2;
+export function gradeSleep(test2, age){
+    var average;
     var num;
+    var adjustor;
     //console.log(test2)
+
+    switch(true) {
+        case age == 0:
+            average = 16;
+            adjustor = 0;
+            break;
+        case age == 1:
+            average = 12;
+            adjustor = 0.4;
+            break;
+        case age == 2:
+            average = 12;
+            adjustor = 0.4;
+            break;
+        case age == 3:
+            average = 10;
+            adjustor = 0.6;
+            break;
+        case age == 4:
+            average = 10;
+            adjustor = 0.6;
+            break;
+        case age == 5:
+            average = 10;
+            adjustor = 0.6;
+            break;
+    }
+
+    var comp = average - test2;
 
     switch(true) {
         case comp <= 0:
             num = 150
             break;
-        case comp >= 1.6 && comp < 3.2:
+        case comp >= (1.6 - adjustor) && comp < (3.2 - adjustor*2):
             num = 214;
             break;
-        case comp >= 3.2 && comp < 4.8:
+        case comp >= (3.2 - adjustor*2) && comp < (4.8 - adjustor*3):
             num = 278;
             break;
-        case comp >= 4.8 && comp < 6.4:
+        case comp >= (4.8 - adjustor*3) && comp < (6.4 - adjustor*4):
             num = 342;
             break;
-        case comp >= 6.4 && comp < 8:
+        case comp >= (6.4 - adjustor*4) && comp < (8 - adjustor*5):
             num = 406;
             break;
-        case comp >= 8 && comp < 9.6:
+        case comp >= (8 - adjustor*5) && comp < (9.6 - adjustor*6):
             num = 470;
             break;
-        case comp >= 9.6 && comp < 11.2:
+        case comp >= (9.6 - adjustor*6) && comp < (11.2 - adjustor*7):
             num = 534;
             break;
-        case comp >= 11.2 && comp < 12.8:
+        case comp >= (11.2 - adjustor*7) && comp < (12.8 - adjustor*8):
             num = 598;
             break;
-        case comp >= 12.8 && comp < 14.4:
+        case comp >= (12.8 - adjustor*8) && comp < (14.4 - adjustor*9):
             num = 662;
             break;
-        case comp >= 14.4 && comp < 16:
+        case comp >= (14.4 - adjustor*9) && comp < (16 - adjustor*10):
             num = 726;
             break;
-        case comp >= 16:
+        case comp >= (16 - adjustor*10):
             num = 790;
             break;
     }
@@ -92,49 +152,122 @@ export function gradeSleep(test2){
 }
 
 // 70 - 105 ounces weekly for a newborn. We'll use that to start functionality
-export function gradeEating(total){
-    var average = 70;
-    var comp = average - total;
+export function gradeEating(total, total2, age){
+    var average;
     var num;
-    console.log(total);
+    var foodType;
+    var adjustor;
+    //console.log(total);
     //console.log(comp);
 
     switch(true) {
-        case comp <= 0:
-            num = 350
+        case age == 0:
+            average = 42;  // Ounces of formula, use the total from breastfeeding
+            foodType = total2;
             break;
-        case comp >= 7 && comp < 14:
-            num = 420;
+        case age == 1:
+            average = 14;   // Meals weekly, use normal total
+            foodType = total;
+            adjustor = 7;
             break;
-        case comp >= 14 && comp < 21:
-            num = 490;
+        case age == 2:
+            average = 21;   
+            foodType = total;
+            adjustor = 0;
             break;
-        case comp >= 21 && comp < 28:
-            num = 560;
+        case age == 3:
+            average = 21;
+            foodType = total;
+            adjustor = 0;
             break;
-        case comp >= 28 && comp < 35:
-            num = 630;
+        case age == 4:
+            average = 21;
+            foodType = total;
+            adjustor = 0;
             break;
-        case comp >= 35 && comp < 42:
-            num = 700;
-            break;
-        case comp >= 42 && comp < 48:
-            num = 770;
-            break;
-        case comp >= 48 && comp < 56:
-            num = 840;
-            break;
-        case comp >= 56 && comp < 63:
-            num = 910;
-            break;
-        case comp >= 63 && comp < 70:
-            num = 980;
-            break;
-        case comp >= 70:
-            num = 1050;
+        case age == 5:
+            average = 21;
+            foodType = total;
+            adjustor = 0;
             break;
     }
-    //.log(num)
+
+    var comp = average - foodType;
+    if(foodType == total2) {
+        switch(true) {
+            case comp <= 0:
+                num = 350
+                break;
+            case comp >= 4.2 && comp < 8.4:
+                num = 420;
+                break;
+            case comp >= 8.4 && comp < 12.6:
+                num = 490;
+                break;
+            case comp >= 12.6 && comp < 16.8:
+                num = 560;
+                break;
+            case comp >= 16.8 && comp < 21:
+                num = 630;
+                break;
+            case comp >= 21 && comp < 25.2:
+                num = 700;
+                break;
+            case comp >= 25.2 && comp < 29.4:
+                num = 770;
+                break;
+            case comp >= 29.4 && comp < 33.6:
+                num = 840;
+                break;
+            case comp >= 33.6 && comp < 37.8:
+                num = 910;
+                break;
+            case comp >= 37.8 && comp < 42:
+                num = 980;
+                break;
+            case comp >= 42:
+                num = 1050;
+                break;
+        }
+    }
+    else{
+        switch(true) {
+            case comp <= 0:
+                num = 350
+                break;
+            case comp >= (2.1 - adjustor) && comp < (4.2 - adjustor):
+                num = 420;
+                break;
+            case comp >= (4.2 - adjustor) && comp < (6.3 - adjustor):
+                num = 490;
+                break;
+            case comp >= (6.3 - adjustor) && comp < (8.4 - adjustor):
+                num = 560;
+                break;
+            case comp >= (8.4 - adjustor) && comp < (10.5 - adjustor):
+                num = 630;
+                break;
+            case comp >= (10.5 - adjustor) && comp < (12.6 - adjustor):
+                num = 700;
+                break;
+            case comp >= (12.6 - adjustor) && comp < (14.7 - adjustor):
+                num = 770;
+                break;
+            case comp >= (14.7 - adjustor) && comp < (16.8 - adjustor):
+                num = 840;
+                break;
+            case comp >= (16.8 - adjustor) && comp < (18.9 - adjustor):
+                num = 910;
+                break;
+            case comp >= (18.9 - adjustor) && comp < (21 - adjustor):
+                num = 980;
+                break;
+            case comp >= (21 - adjustor):
+                num = 1050;
+                break;
+        }
+    }
+    
     var i = num - (780 * .3);
     return i;
 }
