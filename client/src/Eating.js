1abe41256c3dfe7d0f1868b143748fbb0259f04b
TheAstralShade <81994529+TheAstralShade@users.noreply.github.com>
2022-04-04 19:15:35 -0500

    Implemented important entry functionality

--- client/src/Eating.js
+++ client/src/Eating.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-//import {NavBar} from './NavBar.js';
 import Header from "./component/Header";
 import './eating.css';
 import Axios from 'axios';
+let importantbottle = [false, false, false, false, false, false, false]
+let importantbreast = [false, false, false, false, false, false, false]
  
 class Feeding extends Component{
   constructor(){
@@ -139,7 +139,71 @@ class Feeding extends Component{
       total: this.state.bottleEntry1[1] + this.state.bottleEntry2[1] + this.state.bottleEntry3[1] + this.state.bottleEntry4[1] + this.state.bottleEntry5[1] + this.state.bottleEntry6[1] + this.state.bottleEntry7[1]
     }).then((response) => {
         console.log(response);
-  });
+    });
+
+    let bottletemp1 = this.state.bottleEntry1[0]
+    let bottletemp2 = this.state.bottleEntry2[0]
+    let bottletemp3 = this.state.bottleEntry3[0]
+    let bottletemp4 = this.state.bottleEntry4[0]
+    let bottletemp5 = this.state.bottleEntry5[0]
+    let bottletemp6 = this.state.bottleEntry6[0]
+    let bottletemp7 = this.state.bottleEntry7[0]
+    let bottletemp8 = this.state.bottleEntry1[1]
+    let bottletemp9 = this.state.bottleEntry2[1]
+    let bottletemp10 = this.state.bottleEntry3[1]
+    let bottletemp11 = this.state.bottleEntry4[1]
+    let bottletemp12 = this.state.bottleEntry5[1]
+    let bottletemp13 = this.state.bottleEntry6[1]
+    let bottletemp14 = this.state.bottleEntry7[1]
+
+    if (!importantbottle[0]) {
+      bottletemp1 = null
+      bottletemp8 = null
+    }
+    if (!importantbottle[1]) {
+      bottletemp2 = null
+      bottletemp9 = null
+    }
+    if (!importantbottle[2]) {
+      bottletemp3 = null
+      bottletemp10 = null
+    }
+    if (!importantbottle[3]) {
+      bottletemp4 = null
+      bottletemp11 = null
+    }
+    if (!importantbottle[4]) {
+      bottletemp5 = null
+      bottletemp12 = null
+    }
+    if (!importantbottle[5]) {
+      bottletemp6 = null
+      bottletemp13 = null
+    }
+    if (!importantbottle[6]) {
+      bottletemp7 = null
+      bottletemp14 = null
+    }
+
+    Axios.post("http://localhost:5000/importantEntriesEating", {
+      temp1: bottletemp1,
+      temp2: bottletemp2,
+      temp3: bottletemp3,
+      temp4: bottletemp4,
+      temp5: bottletemp5,
+      temp6: bottletemp6,
+      temp7: bottletemp7,
+      temp8: bottletemp8,
+      temp9: bottletemp9,
+      temp10: bottletemp10,
+      temp11: bottletemp11,
+      temp12: bottletemp12,
+      temp13: bottletemp13,
+      temp14: bottletemp14
+    }).then((response) => {
+        console.log(response);
+    });
+
   }
 
   handleClick4 = () => {
@@ -166,7 +230,8 @@ class Feeding extends Component{
         this.state.bottleEntry7[1] = response.data[0].Amount7
       }
   });
-  }
+
+}
 
   handleClick5 = () => {
     Axios.post("http://localhost:5000/eating-breast", {
@@ -187,7 +252,72 @@ class Feeding extends Component{
     }).then((response) => {
         console.log(response);
   });
+
+  let foodtemp1 = this.state.breastEntry1[0]
+  let foodtemp2 = this.state.breastEntry2[0]
+  let foodtemp3 = this.state.breastEntry3[0]
+  let foodtemp4 = this.state.breastEntry4[0]
+  let foodtemp5 = this.state.breastEntry5[0]
+  let foodtemp6 = this.state.breastEntry6[0]
+  let foodtemp7 = this.state.breastEntry7[0]
+  let foodtemp8 = this.state.breastEntry1[1]
+  let foodtemp9 = this.state.breastEntry2[1]
+  let foodtemp10 = this.state.breastEntry3[1]
+  let foodtemp11 = this.state.breastEntry4[1]
+  let foodtemp12 = this.state.breastEntry5[1]
+  let foodtemp13 = this.state.breastEntry6[1]
+  let foodtemp14 = this.state.breastEntry7[1]
+
+  if (!importantbreast[0]) {
+    foodtemp1 = null
+    foodtemp8 = null
   }
+  if (!importantbreast[1]) {
+    foodtemp2 = null
+    foodtemp9 = null
+  }
+  if (!importantbreast[2]) {
+    foodtemp3 = null
+    foodtemp10 = null
+  }
+  if (!importantbreast[3]) {
+    foodtemp4 = null
+    foodtemp11 = null
+  }
+  if (!importantbreast[4]) {
+    foodtemp5 = null
+    foodtemp12 = null
+  }
+  if (!importantbreast[5]) {
+    foodtemp6 = null
+    foodtemp13 = null
+  }
+  if (!importantbreast[6]) {
+    foodtemp7 = null
+    foodtemp14 = null
+  }
+
+  Axios.post("http://localhost:5000/importantEntriesEating", {
+    temp1: foodtemp1,
+    temp2: foodtemp2,
+    temp3: foodtemp3,
+    temp4: foodtemp4,
+    temp5: foodtemp5,
+    temp6: foodtemp6,
+    temp7: foodtemp7,
+    temp8: foodtemp8,
+    temp9: foodtemp9,
+    temp10: foodtemp10,
+    temp11: foodtemp11,
+    temp12: foodtemp12,
+    temp13: foodtemp13,
+    temp14: foodtemp14
+  }).then((response) => {
+      console.log(response);
+  });
+
+
+}
 
   handleClick6 = () => {
     Axios.post("http://localhost:5000/get-eating-breast", {
@@ -231,6 +361,24 @@ class Feeding extends Component{
     this.setState({ breastDay: e.target.value})
   }
 
+  handleCheckClick1(value) {
+    //console.log("Test 1", value)
+    for(var i =0; i < 7; i++) {
+      if (i === value) {
+        importantbottle[i] ? importantbottle[i]=false : importantbottle[i]=true;
+      }
+    }
+  }
+
+  handleCheckClick2(value) {
+    //console.log("Test 2", value)
+    for(var i =0; i < 7; i++) {
+      if (i === value) {
+        importantbreast[i] ? importantbreast[i]=false : importantbreast[i]=true;
+      }
+    }
+  }
+
   render(){
     return (
       <div>
@@ -263,13 +411,41 @@ class Feeding extends Component{
         </div>
         <div className="Week Box">
           <h2>Bottle Records</h2>
-          <h3>{this.state.bottleEntry1[0]} : {this.state.bottleEntry1[1]} Ounces</h3>
-          <h3>{this.state.bottleEntry2[0]} : {this.state.bottleEntry2[1]} Ounces</h3>
-          <h3>{this.state.bottleEntry3[0]} : {this.state.bottleEntry3[1]} Ounces</h3>
-          <h3>{this.state.bottleEntry4[0]} : {this.state.bottleEntry4[1]} Ounces</h3>
-          <h3>{this.state.bottleEntry5[0]} : {this.state.bottleEntry5[1]} Ounces</h3>
-          <h3>{this.state.bottleEntry6[0]} : {this.state.bottleEntry6[1]} Ounces</h3>
-          <h3>{this.state.bottleEntry7[0]} : {this.state.bottleEntry7[1]} Ounces</h3>
+          <h3>
+            <input id="check" type="checkbox" value="false" onClick={() => this.handleCheckClick1(0)}></input>
+            <label for="check">Mark Important</label>&nbsp;&nbsp;&nbsp;
+            {this.state.bottleEntry1[0]} : {this.state.bottleEntry1[1]} Ounces
+          </h3>
+          <h3>
+            <input id="check" type="checkbox" value="false" onClick={() => this.handleCheckClick1(1)}></input>
+            <label for="check">Mark Important</label>&nbsp;&nbsp;&nbsp;
+            {this.state.bottleEntry2[0]} : {this.state.bottleEntry2[1]} Ounces
+          </h3>
+          <h3>
+            <input id="check" type="checkbox" value="false" onClick={() => this.handleCheckClick1(2)}></input>
+            <label for="check">Mark Important</label>&nbsp;&nbsp;&nbsp;
+            {this.state.bottleEntry3[0]} : {this.state.bottleEntry3[1]} Ounces
+          </h3>
+          <h3>
+            <input id="check" type="checkbox" value="false" onClick={() => this.handleCheckClick1(3)}></input>
+            <label for="check">Mark Important</label>&nbsp;&nbsp;&nbsp;
+            {this.state.bottleEntry4[0]} : {this.state.bottleEntry4[1]} Ounces
+          </h3>
+          <h3>
+            <input id="check" type="checkbox" value="false" onClick={() => this.handleCheckClick1(4)}></input>
+            <label for="check">Mark Important</label>&nbsp;&nbsp;&nbsp;
+            {this.state.bottleEntry5[0]} : {this.state.bottleEntry5[1]} Ounces
+          </h3>
+          <h3>
+            <input id="check" type="checkbox" value="false" onClick={() => this.handleCheckClick1(5)}></input>
+            <label for="check">Mark Important</label>&nbsp;&nbsp;&nbsp;
+            {this.state.bottleEntry6[0]} : {this.state.bottleEntry6[1]} Ounces
+          </h3>
+          <h3>
+            <input id="check" type="checkbox" value="false" onClick={() => this.handleCheckClick1(6)}></input>
+            <label for="check">Mark Important</label>&nbsp;&nbsp;&nbsp;
+            {this.state.bottleEntry7[0]} : {this.state.bottleEntry7[1]} Ounces
+          </h3>
         </div>
         <div className="Breastfeed Box">
           <h2>Breastfeeding Entry</h2>
@@ -299,13 +475,41 @@ class Feeding extends Component{
         </div>
         <div className="Day Box">
           <h2>Breastfed Records</h2>
-          <h3>{this.state.breastEntry1[0]} : {this.state.breastEntry1[1]} Minutes</h3>
-          <h3>{this.state.breastEntry2[0]} : {this.state.breastEntry2[1]} Minutes</h3>
-          <h3>{this.state.breastEntry3[0]} : {this.state.breastEntry3[1]} Minutes</h3>
-          <h3>{this.state.breastEntry4[0]} : {this.state.breastEntry4[1]} Minutes</h3>
-          <h3>{this.state.breastEntry5[0]} : {this.state.breastEntry5[1]} Minutes</h3>
-          <h3>{this.state.breastEntry6[0]} : {this.state.breastEntry6[1]} Minutes</h3>
-          <h3>{this.state.breastEntry7[0]} : {this.state.breastEntry7[1]} Minutes</h3>
+          <h3>
+            <input id="check" type="checkbox" value="false" onClick={() => this.handleCheckClick2(0)}></input>
+            <label for="check">Mark Important</label>&nbsp;&nbsp;&nbsp;
+            {this.state.breastEntry1[0]} : {this.state.breastEntry1[1]} Minutes
+          </h3>
+          <h3>
+            <input id="check" type="checkbox" value="false" onClick={() => this.handleCheckClick2(1)}></input>
+            <label for="check">Mark Important</label>&nbsp;&nbsp;&nbsp;
+            {this.state.breastEntry2[0]} : {this.state.breastEntry2[1]} Minutes
+          </h3>
+          <h3>
+            <input id="check" type="checkbox" value="false" onClick={() => this.handleCheckClick2(2)}></input>
+            <label for="check">Mark Important</label>&nbsp;&nbsp;&nbsp;
+            {this.state.breastEntry3[0]} : {this.state.breastEntry3[1]} Minutes
+          </h3>
+          <h3>
+            <input id="check" type="checkbox" value="false" onClick={() => this.handleCheckClick2(3)}></input>
+            <label for="check">Mark Important</label>&nbsp;&nbsp;&nbsp;
+            {this.state.breastEntry4[0]} : {this.state.breastEntry4[1]} Minutes
+          </h3>
+          <h3>
+            <input id="check" type="checkbox" value="false" onClick={() => this.handleCheckClick2(4)}></input>
+            <label for="check">Mark Important</label>&nbsp;&nbsp;&nbsp;
+            {this.state.breastEntry5[0]} : {this.state.breastEntry5[1]} Minutes
+          </h3>
+          <h3>
+            <input id="check" type="checkbox" value="false" onClick={() => this.handleCheckClick2(5)}></input>
+            <label for="check">Mark Important</label>&nbsp;&nbsp;&nbsp;
+            {this.state.breastEntry6[0]} : {this.state.breastEntry6[1]} Minutes
+          </h3>
+          <h3>
+            <input id="check" type="checkbox" value="false" onClick={() => this.handleCheckClick2(6)}></input>
+            <label for="check">Mark Important</label>&nbsp;&nbsp;&nbsp;
+            {this.state.breastEntry7[0]} : {this.state.breastEntry7[1]} Minutes
+          </h3>
         </div>
       </div>
     );
