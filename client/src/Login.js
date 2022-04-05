74cc8ef19083f10c28ac5aa48fec8d28215823f8
Faruq Jimoh-Taiwo <deimosjt@gmail.com>
2022-04-05 00:51:52 -0500

    Implemented ProtectedRoutes- Use of Hooks for login- Hashed/Encrypted Password (mySQL)

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,145 +1,166 @@
-import React, { Component } from 'react';
-//import ReactDOM from 'react-dom';
-//import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import './index.css';
-import Axios from 'axios';
+import React, { Component } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import "./index.css";
 
 class LoginComponent extends Component {
-  constructor(){
-    super()
+  constructor(props) {
+    super(props);
+
     this.state = {
-      showLogin:true,
-      showRegister:false,
-      username: '',
-      password: ''
-    }
+      showLogin: true,
+      showRegister: false,
+      username: "",
+      password: "",
+    };
+    this.handleClick2 = this.props.handleClick;
+    this.handleClick3 = this.props.handleReg;
   }
 
   //Handles switch between
   handleClick = () => {
-    console.log('this is:', this);
-    if(this.state.showLogin == false){
+    console.log("this is:", this);
+
+    if (this.state.showLogin == false) {
       this.setState({
-        showLogin:true,
-        showRegister:false
-      })
-    }
-    else{
+        showLogin: true,
+        showRegister: false,
+      });
+    } else {
       this.setState({
-        showLogin:false,
-        showRegister:true
-      })
+        showLogin: false,
+        showRegister: true,
+      });
     }
-
-  }
-
-  handleClick2 = () => {
-   //Area where login button click is recognized
-   //class's can be misleading, read plain text to identify components if confused
-   Axios.post("http://localhost:5000/login", {
-    username: this.state.username,
-    password: this.state.password,
-  }).then((response) => {
-
-    if(response.data.message) {
-      console.log(response.data.message)
-    }
-    else {
-      console.log(response.data[0].username)
-      window.open("/home")
-    }
-  });
-
-  }
-
-  handleClick3 = () => {
-    Axios.post("http://localhost:5000/register", {
-      usernameSet: this.state.username,
-      passwordSet: this.state.password,
-    }).then((response) => {
-      console.log(response);
-    });
-  }
+  };
 
   handleUsernameChange = (e) => {
-    this.setState({ username: e.target.value})
-  }
+    this.setState({ username: e.target.value });
+  };
 
   handlePasswordChange = (e) => {
-    this.setState({ password: e.target.value})
-  }
+    this.setState({ password: e.target.value });
+  };
 
+  render() {
+    return (
+      <>
+        {this.state.showLogin ? (
+          <div className="row mt-5">
+            <div className="col-md-6 m-auto">
+              <div className="card card-body">
+                <h1 className="text-center mb-3">
+                  <i className="fas fa-sign-in-alt"></i> Login
+                </h1>
 
-  render(){
-    return ( <div class="centered" > 
-    { this.state.showLogin?
-      <div id="LoginBox" class = "LoginCenter">
-        <div class = "LogInTest">Log in</div>
-        <hr class="Line"></hr>
-        <div class="spacer">Space</div>
-        <div class="UserNameTest">Username</div>
-        <div class="UserNameTest">
-        <input 
-          value={this.state.username} 
-          onChange={this.handleUsernameChange}
+                {/* <form> */}
+                <div className="form-group">
+                  <label>Email</label>
+                  <input
+                    type="username"
+                    id="username"
+                    name="username"
+                    className="form-control"
+                    placeholder="Enter Username"
+                    value={this.state.username}
+                    onChange={this.handleUsernameChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Password</label>
+                  <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    className="form-control"
+                    placeholder="Enter Password"
+                    value={this.state.password}
+                    onChange={this.handlePasswordChange}
+                  />
+                </div>
+                <button
+                  className="btn btn-primary btn-block"
+                  onClick={() =>
+                    this.handleClick2(this.state.username, this.state.password)
+                  }
+                >
+                  Login
+                </button>
+                {/* </form> */}
+                <p className="lead mt-4">
+                  No Account?{" "}
+                  <button onClick={this.handleClick}>Register</button>
+                </p>
+              </div>
+            </div>
+          </div>
+        ) : null}
+        {this.state.showRegister ? (
+          <div className="row mt-5">
+            <div className="col-md-6 m-auto">
+              <div className="card card-body">
+                <h1 className="text-center mb-3">
+                  <i className="fas fa-sign-in-alt"></i> Register
+                </h1>
+                <div className="form-group">
+                  {/* <label>Email</label> */}
+                  <input
+                    type="username"
+                    id="username"
+                    name="username"
+                    className="form-control"
+                    placeholder="Enter Email"
+                    value={this.state.username}
+                    onChange={this.handleUsernameChange}
+                  />
+                </div>
+                <div className="form-group">
+                  {/* <label className="form-label">Email</label> */}
+                  <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    className="form-control"
+                    placeholder="Enter Password"
+                    value={this.state.password}
+                    onChange={this.handlePasswordChange}
+                  />
+                </div>
+                <button
+                  className="btn btn-primary btn-block"
+                  onClick={() =>
+                    this.handleClick3(this.state.username, this.state.password)
+                  }
+                >
+                  Register
+                </button>
+                <p className="lead mt-4">
+                  <button
+                    type="button"
+                    className="btn btn-danger"
+                    onClick={this.handleClick}
+                  >
+                    Cancel
+                  </button>
+                </p>
+              </div>
+            </div>
+          </div>
+        ) : null}
+        <ToastContainer
+          position="top-left"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
         />
-        </div>
-          <div class="spacer2">Space</div>
-          <div class="UserNameTest">Password</div>
-          <div class="UserNameTest">
-          <input 
-            type="password" 
-            name="password"
-            value={this.state.password} 
-            onChange={this.handlePasswordChange}
-          />
-        </div>
-        <div class="spacer2">Space</div>
-        <div class="UserNameTest">
-        <button class="ButtonFormat" onClick={this.handleClick2}>Log In</button>
-        <button class="ButtonFormat" onClick={this.handleClick}>Register</button> 
-
-        </div>
-
-
-      </div>
-      :null}
-      {this.state.showRegister?
-      <div id="LoginBox" class = "LoginCenter">
-        <div class = "LogInTest">Register</div>
-        <hr class="Line"></hr>
-        <div class="spacer2">Space</div>
-        <div class="UserNameTest">Email</div>
-        <div class="UserNameTest">
-          <input ></input>
-        </div>
-        <div class="spacer2">Space</div>
-        <div class="UserNameTest">Username</div>
-        <div class="UserNameTest">
-          <input 
-             value={this.state.username} 
-             onChange={this.handleUsernameChange}
-          />
-        </div>
-        <div class="spacer2">Space</div>
-        <div class="UserNameTest">Password</div>
-        <div class="UserNameTest">
-        <input 
-          type="password" 
-          name="password" 
-          value={this.state.password} 
-          onChange={this.handlePasswordChange}
-        />
-        </div>
-        <div class="spacer2">Space</div>
-        <div class="UserNameTest">
-          <button class="ButtonFormat" onClick={this.handleClick3}>Register</button>
-          <button class="ButtonFormat" onClick={this.handleClick}>Cancel</button>
-        </div>
-      </div>
-  :null}
-    </div>
-    )
+      </>
+    );
   }
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,43 +1,274 @@
-import './App.css';
-import React, { Component } from "react";
-import  ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import React, { Fragment, useState } from "react";
+import ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from "react-router-dom";
 import Main from "./Main";
-import {AddChild} from './addchild'
-import LoginComponent from './Login';
-import Bathroom from './bathroom';
-import SleepLog from './sleeplog';
-import Feeding from './Eating';
-import ImportantEntries from './importantEntries';
+import { AddChild } from "./addchild";
+import { toast } from "react-toastify";
+import Axios from "axios";
+import LoginComponent from "./Login";
+import Bathroom from "./bathroom";
+import SleepLog from "./sleeplog";
+import Feeding from "./Eating";
+import ImportantEntries from "./importantEntries";
+import ProctedRoute from "./component/ProtectedRoute";
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      isSignedIn: false,
-      username:""
-    }
-  }
-  render() {
+function App() {
+  const [isSignedIn, setSigned] = useState(false);
+  const navigate = useNavigate();
+  console.log(isSignedIn);
+  const handleClick2 = async (username, password) => {
+    Axios.post("http://localhost:5000/login", {
+      username,
+      password,
+    })
+      .then((response) => {
+        console.log(response);
+        toast.success(` Welcome ${response.data}`, {
+          position: "top-left",
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+        setSigned(true);
+        setTimeout(() => {
+          navigate("/home");
+        }, 3000);
+      })
+
+      .catch((error) => {
+        toast.error(error.response.data.message, {
+          position: "top-left",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      });
+  };
+  const handleClick3 = async (username, password) => {
+    Axios.post("http://localhost:5000/register", {
+      usernameSet: username,
+      passwordSet: password,
+    })
+      .then((response) => {
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log(err.response);
+        toast.error(err.response.data, {
+          position: "top-left",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      });
+  };
+
   return (
-    <Router>
+    <Fragment>
       <div>
         <Routes>
-          <Route exact path="/" element={<LoginComponent comp={this.sta} />} />
-          <Route path ="/home" element={<Main />} />
-          <Route path ="/addnewchild" element={<AddChild />} />
-          <Route path ="/bathroom" element={<Bathroom />} />
-          <Route path ="/eating" element={<Feeding />} />
-          <Route path ="/sleeping" element={<SleepLog />} />
-          <Route path ="/important" element={<ImportantEntries />}/>
+          <Route
+            exact
+            path="/"
+            element={
+              <LoginComponent
+                handleClick={handleClick2}
+                handleReg={handleClick3}
+              />
+            }
+          />
+
+          <Route
+            path="/home"
+            element={
+              <ProctedRoute isSignedIn={isSignedIn}>
+                <Main />
+              </ProctedRoute>
+            }
+          />
+          <Route
+            path="/addnewchild"
+            element={
+              <ProctedRoute isSignedIn={isSignedIn}>
+                <AddChild />
+              </ProctedRoute>
+            }
+          />
+          <Route
+            path="/bathroom"
+            element={
+              <ProctedRoute isSignedIn={isSignedIn}>
+                <Bathroom />
+              </ProctedRoute>
+            }
+          />
+          <Route
+            path="/eating"
+            element={
+              <ProctedRoute isSignedIn={isSignedIn}>
+                <Feeding />
+              </ProctedRoute>
+            }
+          />
+          <Route
+            path="/sleeping"
+            element={
+              <ProctedRoute isSignedIn={isSignedIn}>
+                <SleepLog />
+              </ProctedRoute>
+            }
+          />
+          <Route
+            path="/important"
+            element={
+              <ProctedRoute isSignedIn={isSignedIn}>
+                <ImportantEntries />
+              </ProctedRoute>
+            }
+          />
         </Routes>
       </div>
-    </Router>
+    </Fragment>
   );
-  }
 }
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       isSignedIn: false,
+//       username: "",
+//       password: "",
+//     };
+//   }
+//   handleClick2 = (username, password) => {
+//     console.log(username, password);
+//     Axios.post("http://localhost:5000/login", {
+//       username: username,
+//       password: password,
+//     })
+//       .then((response, err) => {
+//         this.setState({ isSignedIn: true });
+//         console.log(response);
+//         toast.success(` Welcome ${response.data}`, {
+//           position: "top-left",
+//           autoClose: 2000,
+//           hideProgressBar: false,
+//           closeOnClick: true,
+//           pauseOnHover: true,
+//           draggable: true,
+//           progress: undefined,
+//         });
+//         setTimeout(() => {
+//           this.props.history.push("/home");
+//         }, 3000);
+//       })
+//       .catch((err) => {
+//         console.log(err.response);
+//         toast.error(err.response.data.message, {
+//           position: "top-left",
+//           autoClose: 5000,
+//           hideProgressBar: false,
+//           closeOnClick: true,
+//           pauseOnHover: true,
+//           draggable: true,
+//           progress: undefined,
+//         });
+//       });
+//   };
+
+//   render() {
+//     return (
+//       <Router>
+//         <Fragment>
+//           <div>
+//             <Routes>
+//               <Route
+//                 exact
+//                 path="/"
+//                 element={
+//                   <LoginComponent
+//                     comp={this.state}
+//                     handleClick={this.handleClick2}
+//                   />
+//                 }
+//               />
+
+//               <Route
+//                 path="/home"
+//                 element={
+//                   <ProctedRoute isSignedIn={this.state.isSignedIn}>
+//                     <Main />
+//                   </ProctedRoute>
+//                 }
+//               />
+//               <Route
+//                 path="/addnewchild"
+//                 element={
+//                   <ProctedRoute isSignedIn={this.state.isSignedIn}>
+//                     <AddChild />
+//                   </ProctedRoute>
+//                 }
+//               />
+//               <Route
+//                 path="/bathroom"
+//                 element={
+//                   <ProctedRoute isSignedIn={this.state.isSignedIn}>
+//                     <Bathroom />
+//                   </ProctedRoute>
+//                 }
+//               />
+//               <Route
+//                 path="/eating"
+//                 element={
+//                   <ProctedRoute isSignedIn={this.state.isSignedIn}>
+//                     <Feeding />
+//                   </ProctedRoute>
+//                 }
+//               />
+//               <Route
+//                 path="/sleeping"
+//                 element={
+//                   <ProctedRoute isSignedIn={this.state.isSignedIn}>
+//                     <SleepLog />
+//                   </ProctedRoute>
+//                 }
+//               />
+//               <Route
+//                 path="/important"
+//                 element={
+//                   <ProctedRoute isSignedIn={this.state.isSignedIn}>
+//                     <ImportantEntries />
+//                   </ProctedRoute>
+//                 }
+//               />
+//             </Routes>
+//           </div>
+//         </Fragment>
+//       </Router>
+//     );
+//   }
+// }
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
 );
+
+export default App;
